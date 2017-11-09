072112f121f9a24518c16d6d15aa30140706d94b
Thomas Nguyen <tnguyen@veritone.com>
2017-11-08 16:48:19 -0800

    Add Portuguese language pack

--- container-scripts/tessdata_download.sh
+++ container-scripts/tessdata_download.sh
@@ -8,4 +8,6 @@ wget -O ${TESSDATA_PREFIX}/equ.traineddata https://github.com/tesseract-ocr/tess
 wget -O ${TESSDATA_PREFIX}/eng.traineddata https://github.com/tesseract-ocr/tessdata/raw/4.00/eng.traineddata
 # jpn Japanese
 wget -O ${TESSDATA_PREFIX}/jpn.traineddata https://github.com/tesseract-ocr/tessdata/raw/4.00/jpn.traineddata
+# por Portuguese
+wget -O ${TESSDATA_PREFIX}/por.traineddata https://github.com/tesseract-ocr/tessdata/raw/4.00/por.traineddata
 # other languages: https://github.com/tesseract-ocr/tesseract/wiki/Data-Files
